34409d6f8bfe7f5a1b63699596e2bf017fd90674
Harry Morris <41hmorris@gmail.com>
2023-10-12 17:17:50 +0100

    tyring to fix quanity range

--- things/models.py
+++ things/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, default='', unique=True, blank=False)
     description = models.TextField(max_length=120, default='', unique=False, blank = True)
-    quantity = models.IntegerField(default=0, unique=False)
+    quantity = models.IntegerField(validators=[
+        MinValueValidator(0, message='Quantity must not be negative'),
+        MaxValueValidator(100, message='Quantity must not be greater than 100')
+    ])
 
 
