25e10a33c33d6ee760181c9ec2b698e761942a21
Harry Morris <41hmorris@gmail.com>
2023-10-12 17:23:25 +0100

    changed text field to char field

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, default='', unique=True, blank=False)
-    description = models.TextField(max_length=119, default='', unique=False, blank = True)
+    description = models.CharField(max_length=120, default='', unique=False, blank = True)
     quantity = models.IntegerField(validators=[
         MinValueValidator(0, message='Quantity must not be negative'),
         MaxValueValidator(100, message='Quantity must not be greater than 100')
