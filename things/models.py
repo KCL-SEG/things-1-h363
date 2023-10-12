5ecb61b8c0ea57e17ababbcb6ee5bb38a7aeb3d6
Harry Morris <41hmorris@gmail.com>
2023-10-12 17:02:56 +0100

    unique

--- things/models.py
+++ things/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=100, default='')
-    description = models.TextField(default='')
-    quantity = models.IntegerField(default=0)
+    name = models.CharField(max_length=100, default='', Unique=True)
+    description = models.TextField(default='', Unique=False)
+    quantity = models.IntegerField(default=0, Unique=False)
 
 
