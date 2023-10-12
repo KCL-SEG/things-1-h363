7fa8d6c0df4d91671daff4325be7cee850d0b2ea
Harry Morris <41hmorris@gmail.com>
2023-10-12 17:14:36 +0100

    created migration for obejct

--- things/models.py
+++ things/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=100, default='', unique=True)
-    description = models.TextField(default='', unique=False)
+    name = models.CharField(max_length=30, default='', unique=True, blank=False)
+    description = models.TextField(max_length=120, default='', unique=False, blank = True)
     quantity = models.IntegerField(default=0, unique=False)
 
 
